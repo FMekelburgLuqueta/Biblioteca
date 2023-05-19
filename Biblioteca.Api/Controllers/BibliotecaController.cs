00fec184f219004d107b649b64b50305b4ecd9e6
FELIPE MEKELBURG LUQUETA <luqueta.6907@aluno.pr.senac.br>
2023-05-19 20:59:57 -0300

    Commit Inicial

---
+++ Biblioteca.Api/Controllers/BibliotecaController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Biblioteca.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BibliotecaController : ControllerBase
+    {
+    }
+}
---
+++ Biblioteca.Api/Handler/BibliotecaHandler.cs
@@ -0,0 +1,6 @@
+﻿namespace Biblioteca.Api.Handler
+{
+    public class BibliotecaHandler
+    {
+    }
+}
